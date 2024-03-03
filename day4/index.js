b11bc997f65b3878d6622efab18f483ec15745e0
maxkat33 <max.katilius@gmail.com>
2024-03-03 17:26:57 +1100

    day4 clean-up

--- day4/index.js
+++ day4/index.js
@@ -11,12 +11,14 @@ function createMD5Hash(input) {
 
 // console.log(createMD5Hash(puzzleKey + "3683616"))
 
-function findPuzzleHash(puzzleKey){
+function findPuzzleHash(puzzleKey, length){
 
     let i = 0;
     let hash = "";
 
-    while(hash.substring(0, 6) !== "000000") {
+    const target = '0'.repeat(length)
+
+    while(hash.substring(0, length) !== target) {
         i++;
         hash = createMD5Hash(puzzleKey + i)
     }
@@ -24,10 +26,14 @@ function findPuzzleHash(puzzleKey){
     return { i, hash}
 }
 
-const puzzleHash = findPuzzleHash(puzzleKey)
 
-console.time('hashing');
-console.log(puzzleHash)
-console.timeEnd('hashing');
+console.time('5ZeroesHashRuntime');
+const puzzleHash5 = findPuzzleHash(puzzleKey, 5)
+console.log(`The lowest number combined with the secret key to create an MD5 hash that begins with 5 zeroes is ${puzzleHash5.i} and the resulting hash is ${puzzleHash5.hash}`)
+console.timeEnd('5ZeroesHashRuntime');
 
 
+console.time('6ZeroesHashRuntime');
+const puzzleHash6 = findPuzzleHash(puzzleKey, 6)
+console.log(`The lowest number combined with the secret key to create an MD5 hash that begins with 6 zeroes is ${puzzleHash6.i} and the resulting hash is ${puzzleHash6.hash}`)
+console.timeEnd('6ZeroesHashRuntime');
